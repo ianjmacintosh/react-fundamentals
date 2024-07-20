ce26226bc615e1f56cd9eeb2d70f8260cdfcaa9f
Ian MacIntosh <ian@ianjmacintosh.com>
2024-07-20 22:32:00 +0000

    Solve exercise 6.02

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,7 +4,9 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  const inputRef = React.useRef('')
+  const inputRef = React.useRef()
+  const submitButtonRef = React.useRef()
+  const [error, setError] = React.useState()
 
   const handleSubmit = event => {
     event.preventDefault()
@@ -12,13 +14,41 @@ function UsernameForm({onSubmitUsername}) {
     onSubmitUsername(inputRef.current.value)
   }
 
+  const handleChange = event => {
+    let value = event.target.value
+    const isValid = value === value.toLowerCase()
+
+    if (!isValid) {
+      setError('Username must be lower case')
+      submitButtonRef.current.disabled = true
+    } else {
+      setError(null)
+      submitButtonRef.current.disabled = false
+    }
+  }
+
+  //   💰 This one's a little more tricky, so here are a few things you need to do to make this work:
+
+  // Create a handleChange function that accepts the change event and uses event.target.value to get the value of the input. Remember this event will be triggered on the input, not the form.
+  // Use the value of the input to determine whether there's an error. There's an error if the user typed any upper-case characters. You can check this really easily via const isValid = value === value.toLowerCase()
+  // If there's an error, set the error state to 'Username must be lower case'. (💰 here's how you do that: setError(isValid ? null : 'Username must be lower case')) and disable the submit button.
+  // Finally, display the error in an element
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="username">Username:</label>
-        <input type="text" id="usernameInput" ref={inputRef} />
+        <input
+          type="text"
+          id="usernameInput"
+          ref={inputRef}
+          onChange={handleChange}
+        />
       </div>
-      <button type="submit">Submit</button>
+      {error ? <p>{error}</p> : ''}
+      <button type="submit" ref={submitButtonRef}>
+        Submit
+      </button>
     </form>
   )
 }
