a390ef82779694fe0c60d5854da8cb10b902f89b
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-10-15 22:02:39 +0000

    Set up 10.01

--- playground/index.tsx
+++ playground/index.tsx
@@ -1,128 +1,44 @@
+import { useState } from 'react'
 import { createRoot } from 'react-dom/client'
-import {
-	ErrorBoundary,
-	useErrorBoundary,
-	type FallbackProps,
-} from 'react-error-boundary'
 
-function OnboardingForm() {
-	const { showBoundary } = useErrorBoundary()
-	return (
-		<form
-			action="api/onboarding"
-			method="POST"
-			encType="multipart/form-data"
-			onSubmit={event => {
-				try {
-					event.preventDefault()
-					const formData = new FormData(event.currentTarget)
-					console.log(Object.fromEntries(formData))
-					const accountType = formData.get('accounType') as string
-					console.log(accountType.toUpperCase())
-				} catch (error: unknown) {
-					showBoundary(error)
-				}
-			}}
-		>
-			<input type="hidden" name="orgId" value="123" />
-			<div>
-				<label htmlFor="accountTypeSelection">Account Type:</label>
-				<select
-					id="accountTypeSelection"
-					name="accountType"
-					defaultValue="student"
-				>
-					<option value="">--Please select an option--</option>
-					<option value="admin">Admin</option>
-					<option value="teacher">Teacher</option>
-					<option value="parent">Parent</option>
-					<option value="student">Student</option>
-				</select>
-			</div>
-			<div>
-				<label htmlFor="usernameInput">Username:</label>
-				<input id="usernameInput" name="username" />
-			</div>
-			<div>
-				<label htmlFor="passwordInput">Password:</label>
-				<input id="passwordInput" name="password" type="password" />
-			</div>
-			<div>
-				<label htmlFor="ageInput">Age:</label>
-				<input
-					id="ageInput"
-					name="age"
-					type="number"
-					min="0"
-					max="200"
-					defaultValue={18}
-				/>
-			</div>
-			<div>
-				<label htmlFor="photoInput">Photo:</label>
-				<input id="photoInput" name="photo" type="file" accept="image/*" />
-			</div>
-			<div>
-				<label htmlFor="colorInput">Favorite Color:</label>
-				<input
-					id="colorInput"
-					name="color"
-					type="color"
-					defaultValue="#002E5D"
-				/>
-			</div>
-			<fieldset>
-				<legend>Visibility:</legend>
-				<label>
-					<input name="visibility" type="radio" value="public" defaultChecked />
-					Public
-				</label>
-				<label>
-					<input name="visibility" type="radio" value="private" />
-					Private
-				</label>
-			</fieldset>
-			<div>
-				<label>
-					<input name="waiver" type="checkbox" defaultChecked />
-					Waiver Signed
-				</label>
-			</div>
-			<div>
-				<label htmlFor="startDateInput">Start Date:</label>
-				<input
-					id="startDateInput"
-					name="startDate"
-					type="date"
-					defaultValue={new Date().toISOString().slice(0, 10)}
-				/>
-			</div>
-			<button type="submit">Submit</button>
-		</form>
-	)
-}
-
-// 🐨 accept the resetErrorBoundary prop here and call it when the user clicks
-// the "Try again" button
-function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
-	return (
-		<div role="alert">
-			There was an error:{' '}
-			<pre style={{ color: 'red', whiteSpace: 'normal' }}>{error.message}</pre>
-			<button onClick={resetErrorBoundary}>Try again</button>
-			{/*
-				🐨 Add a "Try again" button. Assign the resetErrorBoundary function to its onClick prop.
-				💰 For more on the onClick event handler, visit: 📜 https://react.dev/learn/responding-to-events
-			*/}
-		</div>
-	)
-}
+const allItems = [
+	{ id: 'apple', value: '🍎 apple' },
+	{ id: 'orange', value: '🍊 orange' },
+	{ id: 'grape', value: '🍇 grape' },
+	{ id: 'pear', value: '🍐 pear' },
+]
 
 function App() {
+	const [items, setItems] = useState(allItems)
+
+	function addItem() {
+		const itemIds = items.map(i => i.id)
+		const itemToAdd = allItems.find(i => !itemIds.includes(i.id))
+		if (itemToAdd) setItems([...items, itemToAdd])
+	}
+
+	function removeItem(id: string) {
+		setItems(items.filter(i => i.id !== id))
+	}
+
 	return (
-		<ErrorBoundary FallbackComponent={ErrorFallback}>
-			<OnboardingForm />
-		</ErrorBoundary>
+		<div className="keys">
+			<button disabled={items.length >= allItems.length} onClick={addItem}>
+				add item
+			</button>
+			<ul>
+				{items.map(item => (
+					// 🐨 add a key prop to the <li> below. Set it to item.id
+					// 💣 you can then delete this eslint-disable line:
+					// eslint-disable-next-line react/jsx-key
+					<li>
+						<button onClick={() => removeItem(item.id)}>remove</button>{' '}
+						<label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
+						<input id={`${item.id}-input`} defaultValue={item.value} />
+					</li>
+				))}
+			</ul>
+		</div>
 	)
 }
 
