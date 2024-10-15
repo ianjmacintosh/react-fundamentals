4f584fe2b4755ed97e90adfb00e5af3b57b39ead
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2024-10-15 22:45:14 +0000

    Set up 10.02

--- playground/index.tsx
+++ playground/index.tsx
@@ -1,42 +1,128 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 
-const allItems = [
-	{ id: 'apple', value: '🍎 apple' },
-	{ id: 'orange', value: '🍊 orange' },
-	{ id: 'grape', value: '🍇 grape' },
-	{ id: 'pear', value: '🍐 pear' },
-]
-
 function App() {
-	const [items, setItems] = useState(allItems)
+	const [items, setItems] = useState([
+		{ id: 'apple', value: '🍎 apple' },
+		{ id: 'orange', value: '🍊 orange' },
+		{ id: 'grape', value: '🍇 grape' },
+		{ id: 'pear', value: '🍐 pear' },
+	])
 
-	function addItem() {
-		const itemIds = items.map(i => i.id)
-		const itemToAdd = allItems.find(i => !itemIds.includes(i.id))
-		if (itemToAdd) setItems([...items, itemToAdd])
-	}
+	const [autoShuffle, setAutoShuffle] = useState(true)
 
-	function removeItem(id: string) {
-		setItems(items.filter(i => i.id !== id))
+	useEffect(() => {
+		if (autoShuffle) {
+			const id = setInterval(() => setItems(shuffle), 1000)
+			return () => clearInterval(id)
+		}
+	}, [autoShuffle])
+
+	function getChangeHandler(item: (typeof items)[number]) {
+		return (event: React.ChangeEvent<HTMLInputElement>) => {
+			const newValue = event.currentTarget.value
+			setItems(allItems =>
+				allItems.map(i => ({
+					...i,
+					value: i.id === item.id ? newValue : i.value,
+				})),
+			)
+		}
 	}
 
 	return (
 		<div className="keys">
-			<button disabled={items.length >= allItems.length} onClick={addItem}>
-				add item
-			</button>
-			<ul>
-				{items.map(item => (
-					<li key={item.id}>
-						<button onClick={() => removeItem(item.id)}>remove</button>{' '}
-						<label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
-						<input id={`${item.id}-input`} defaultValue={item.value} />
-					</li>
-				))}
-			</ul>
+			<main>
+				<div>
+					<h1>Without a key</h1>
+					<ul style={{ display: 'flex', gap: '10px' }}>
+						{items.map((item, index) => (
+							// 🐨 add a key prop to the <li> below. Set it to item.id
+							// 💣 you can then delete this eslint-disable line:
+							// eslint-disable-next-line react/jsx-key
+							<li>
+								<label htmlFor={`no-key-${item.id}-input`}>
+									No key #{index + 1}
+								</label>
+								<input
+									id={`no-key-${item.id}-input`}
+									className={`${item.id}-input`}
+									value={item.value}
+									onChange={getChangeHandler(item)}
+								/>
+							</li>
+						))}
+					</ul>
+				</div>
+				<div>
+					<h1>With array index as key</h1>
+					<ul style={{ display: 'flex', gap: '10px' }}>
+						{items.map((item, index) => (
+							// 🐨 change the key prop below to item.id (something that actually represents the item)
+							<li key={index}>
+								<label htmlFor={`index-key-${item.id}-input`}>
+									Index key #{index + 1}
+								</label>
+								<input
+									id={`index-key-${item.id}-input`}
+									className={`${item.id}-input`}
+									value={item.value}
+									onChange={getChangeHandler(item)}
+								/>
+							</li>
+						))}
+					</ul>
+				</div>
+				<div>
+					<h1>With a proper key</h1>
+					<ul style={{ display: 'flex', gap: '10px' }}>
+						{items.map((item, index) => (
+							<li key={item.id}>
+								<label htmlFor={`proper-key-${item.id}-input`}>
+									Proper key #{index + 1}
+								</label>
+								<input
+									id={`proper-key-${item.id}-input`}
+									className={`${item.id}-input`}
+									value={item.value}
+									onChange={getChangeHandler(item)}
+								/>
+							</li>
+						))}
+					</ul>
+				</div>
+			</main>
+			<aside style={{ marginTop: '40px' }}>
+				<div style={{ alignItems: 'center', display: 'flex', gap: '8px' }}>
+					<input
+						id="autoshuffle"
+						type="checkbox"
+						checked={autoShuffle}
+						onChange={event => setAutoShuffle(event.target.checked)}
+					/>
+					<label htmlFor="autoshuffle">Auto-shuffle inputs</label>
+				</div>
+			</aside>
 		</div>
 	)
+}
+
+function shuffle<ArrayType>(originalArray: Array<ArrayType>) {
+	const array = [...originalArray]
+	let currentIndex = array.length
+	let temporaryValue
+	let randomIndex
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex)
+		currentIndex -= 1
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex]
+		array[currentIndex] = array[randomIndex]
+		array[randomIndex] = temporaryValue
+	}
+	return array
 }
 
 const rootEl = document.createElement('div')
